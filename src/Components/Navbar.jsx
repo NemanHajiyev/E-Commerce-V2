2f3e811b9f3ec3df3e5cabd7a06f4fe7b110baa1
Neman Haciyev <zaraganq@gmail.com>
2025-01-24 11:10:16 +0400

    Navbar Compelted

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -8,12 +8,12 @@ const Navbar = () => {
         <nav className='navbar'>
             <div className='navbar-div'>
                 <div className='navbar-logo'>
-                    <Link to="/">E-Shop</Link>
+                    <Link to="/" style={{ color: "black" }}>E-Shop</Link>
                 </div>
-                <div>
+                <div style={{ width: "60%" }}>
                     <form className='navbar-form'>
                         <input type="text" placeholder='Search Products ...' />
-                        <FaSearch></FaSearch>
+                        <FaSearch />
                     </form>
                 </div>
                 <div className='navbar-right'>
@@ -27,6 +27,12 @@ const Navbar = () => {
                         <FaUser />
                     </button>
                 </div>
+            </div>
+            <div className='Pages-link'>
+                <Link to="/" className='Link'>Home</Link>
+                <Link to="/" className='Link'>Shop</Link>
+                <Link to="/" className='Link'>Contact</Link>
+                <Link to="/" className='Link'>About</Link>
             </div>
         </nav>
     )
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,15 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import CssBaseline from '@mui/material/CssBaseline';
+import Container from '@mui/material/Container';
+
 
 createRoot(document.getElementById('root')).render(
-  <div className='General-Div'>
-    <App />
-  </div>,
-)
+  <div >
+    <CssBaseline />
+    <Container maxWidth="lg">
+      <App />
+    </Container>
+  </div>
+);
