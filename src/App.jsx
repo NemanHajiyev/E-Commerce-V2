f3d129251cee5758fafcfb25b021673ac2a7dfa1
Neman Haciyev <zaraganq@gmail.com>
2025-01-25 10:38:24 +0400

    Page Container Style Update

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import Home from "./Pages/Home"
 import Shop from "./Pages/Shop"
 import './index.css';
 import Cart from "./Pages/Cart"
+import CssBaseline from '@mui/material/CssBaseline';
+import Container from '@mui/material/Container';
 
 
 
@@ -14,13 +16,14 @@ function App() {
     <div style={{ height: '100vh' }}>
       <BrowserRouter>
         <Navbar />
-        <div className="general">
+        <CssBaseline />
+        <Container Container maxWidth="lg" >
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/shop" element={<Shop />} />
             <Route path="/cart" element={<Cart />} />
           </Routes>
-        </div>
+        </Container>
         <Footer />
       </BrowserRouter>
     </div>
--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,12 @@
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
-import CssBaseline from '@mui/material/CssBaseline';
-import Container from '@mui/material/Container';
 import { store } from './Redux/Store.jsx'
 import { Provider } from 'react-redux'
 
 
 createRoot(document.getElementById('root')).render(
-  <div >
-    <CssBaseline />
-    <Provider store={store}>
-      <App />
-    </Provider>,
-  </div>
+  <Provider store={store}>
+    <App />
+  </Provider>,
 );
