299f5e71dbe1ff19cdac5e2ad90ffdfb1dae8b20
Neman Haciyev <zaraganq@gmail.com>
2025-01-30 18:40:26 +0400

    Shop-page Paginate add

--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -1,24 +1,57 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { setProducts } from '../Redux/productSlice';
 import { mockData } from '../MockData/mockData';
 import Product from '../Pages/Product';
-import '../Styles/Products.css'
+import '../Styles/Products.css';
+import ReactPaginate from 'react-paginate';
+
 
 
 const Shop = () => {
     const dispatch = useDispatch();
-    const products = useSelector(state => state.product.products)
+    const { products } = useSelector(state => state.product)
+    const [itemOffset, setItemOffset] = useState(0);
+
+    const itemsPerPage = 10
+    const endOffset = itemOffset + itemsPerPage;
+    console.log(`Loading items from ${itemOffset} to ${endOffset}`);
+    const currentItems = products.slice(itemOffset, endOffset);
+    const pageCount = Math.ceil(products.length / itemsPerPage);
+    const handlePageClick = (event) => {
+        const newOffset = (event.selected * itemsPerPage) % products.length;
+        console.log(
+            `User requested page number ${event.selected}, which is offset ${newOffset}`
+        );
+        setItemOffset(newOffset);
+    };
+
+
 
     useEffect(() => {
         dispatch(setProducts(mockData))
     }, [])
 
+
     return (
-        <div className='products'>
-            {products?.map((product, index) => (
-                <Product getData={product} key={index} />
-            ))}
+        <div >
+            <div className='products'>
+                {currentItems?.map((product, index) => (
+                    <Product getData={product} key={index} />
+                ))}
+            </div>
+            <div className='paginaton-div'>
+                <ReactPaginate
+                    className='paginate'
+                    breakLabel="..."
+                    nextLabel=">"
+                    onPageChange={handlePageClick}
+                    pageRangeDisplayed={5}
+                    pageCount={pageCount}
+                    previousLabel="<"
+                    renderOnZeroPageCount={null}
+                />
+            </div>
         </div>
     )
 }
